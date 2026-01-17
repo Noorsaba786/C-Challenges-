4eaa040529d97af96645a046ca6d0b64e81d9b6a
saba <noorsabasafi007@gmail.com>
2026-01-17 23:47:03 +0100

    new modfi

---
+++ C– First Week Challenge/Q3Td.c
@@ -0,0 +1,16 @@
+// Ecrire un programme qui permute 2 valeurs sans utilisation de la variable tmp 
+
+#include <stdio.h>
+int main()
+{
+    int a = 5;
+    int b = 10;
+
+    printf("Avant permutation: a = %d, b = %d\n", a, b);
+
+    a  = a + b; // a devient 15
+    b  = a - b; // b devient 5
+    a  = a - b; // a devient 10         
+    printf("Après permutation: a = %d, b = %d\n", a, b);
+    return 0;
+}
