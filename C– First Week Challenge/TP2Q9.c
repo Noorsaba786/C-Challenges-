93b7b3fd80a1a5d433727af54df5301a9e6ed642
saba <noorsabasafi007@gmail.com>
2026-02-02 23:05:35 +0100

    new modif

---
+++ C– First Week Challenge/TP2Q9.c
@@ -0,0 +1,33 @@
+/*
+
+Ecrivez une fonction (toujours r´ecursive !) qui recherche la valeur minimum d’un tableau.
+La fonction doit retourner l’indice de la case qui contient la plus petite valeur.
+Astuce : il est possible de fixer la taille d’un tableau `a l’aide de la macro #define, vue en cours.
+Nous appelerons SIZE MAX la taille d’un tableau. Pour les premiers tests, utilisez des petites
+tailles (disons une vingtaine de valeurs). */
+
+#include <stdio.h>
+#define SIZE_MAX 20
+int findMinIndex(int arr[], int n, int index, int minIndex)
+{
+    if (index == n)
+        return minIndex;
+
+    if (arr[index] < arr[minIndex])
+        minIndex = index;
+
+    return findMinIndex(arr, n, index + 1, minIndex);
+}
+int main()
+{
+    int arr[SIZE_MAX], n, minIndex;
+
+    printf("Enter the number of elements in the array (max %d): ", SIZE_MAX);
+    scanf("%d", &n);
+
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++)
+    {
+
+        scanf("%d", &arr[i]);
+    }
